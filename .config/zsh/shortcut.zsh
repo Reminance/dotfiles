42cc85b1a60b84da6c7df0fe95c5575385975600
Reminance <872666026@qq.com>
2021-07-06 00:55:02 +0800

    update

--- .config/zsh/shortcut.zsh
+++ .config/zsh/shortcut.zsh
@@ -1,32 +1,38 @@
 # toggle background/foreground
 fancy-ctrl-z () {
-  if [[ $#BUFFER -eq 0 ]]; then
+if [[ $#BUFFER -eq 0 ]]; then
     BUFFER="fg"
     zle accept-line
-  else
+else
     zle push-input
     zle clear-screen
-  fi
+fi
 }
 zle -N fancy-ctrl-z
-bindkey '^Z' fancy-ctrl-z
+bindkey '^z' fancy-ctrl-z
 
 # fkill - kill process
 fkill() {
-  local pid
-  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
-
-  if [ "x$pid" != "x" ]
-  then
-	echo $pid | xargs sudo kill -${1:-9}
-  fi
+    local pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+    [ "x$pid" != "x" ] && echo $pid | xargs sudo kill -${1:-9}
+    zle reset-prompt
 }
+zle -N fkill
+bindkey '^Q' fkill
+
+# edit config file
+edit-config-file () {
+    local files=($(cat ~/dotfiles/config-file.list | fzf --query="$1" --select-1 --exit-0))
+    [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+    zle reset-prompt
+}
+zle -N edit-config-file
+bindkey '\ee' edit-config-file
 
 # file search&edit
 fe() {
-  local files
-  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
-  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+    IFS=$'\n' local files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+    [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
 }
 
 # file directory&enter; won't use this function, use sps fd instead
@@ -41,22 +47,22 @@ fe() {
 # `tm` will allow you to select your tmux session via fzf.
 # `tm irc` will attach to the irc session (if it exists), else it will create it.
 tm() {
-  [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
-  if [ $1 ]; then
-	tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
-  fi
-  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
+    [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
+    if [ $1 ]; then
+        tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
+    fi
+    session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
 }
 
 # fs [FUZZY PATTERN] - Select selected tmux session
 #   - Bypass fuzzy finder if there's only one match (--select-1)
 #   - Exit if there's no match (--exit-0)
 fs() {
-  local session
-  session=$(tmux list-sessions -F "#{session_name}" | \
-    fzf --query="$1" --select-1 --exit-0) &&
-  tmux switch-client -t "$session"
-}
+    local session
+    session=$(tmux list-sessions -F "#{session_name}" | \
+        fzf --query="$1" --select-1 --exit-0) &&
+        tmux switch-client -t "$session"
+    }
 
 # sudo pacman -S lf
 # A terminal file manager inspred by ranger written in Go
