78a87409c32ee0384d5824bd17356fbdb5f24e95
xucheng <872666026@qq.com>
2021-08-15 18:08:08 +0800

    update

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -5,7 +5,7 @@ export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border --inline-info'
 
 #To apply the command to CTRL-T as well
 # export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_CTRL_T_COMMAND="fd -t f -H -I"
+export FZF_CTRL_T_COMMAND="fd -t f -H -I --exclude \".git\""
 
 # export FZF_COMPLETION_TRIGGER='**'
 
@@ -13,7 +13,7 @@ export FZF_CTRL_T_COMMAND="fd -t f -H -I"
 export FZF_ALT_C_COMMAND="fd -t d --hidden --follow --exclude \".git\""
 
 # for preview
-export FZF_CTRL_T_OPTS='--preview "bat --style=numbers --color=always --line-range :200 {}"'
+export FZF_CTRL_T_OPTS='--bind "ctrl-y:preview-up,ctrl-e:preview-down,?:toggle-preview" --preview "bat --style=numbers --color=always --line-range :200 {}"'
 export FZF_ALT_C_OPTS='--preview "tree -C {} | head -50"'
 
 export FZF_TMUX=1
