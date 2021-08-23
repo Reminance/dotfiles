baff3434552e459539d870607a741bb1d3ea38f0
xucheng <872666026@qq.com>
2021-08-23 22:52:57 +0800

    update

--- bin/mu4eSetup.sh
+++ bin/mu4eSetup.sh
@@ -6,5 +6,5 @@ mkdir -p ~/email/work
 gpg2 --output .mbsyncpass.gpg --symmetric .mbsyncpass
 gpg2 --output .authinfo.gpg --symmetric .authinfo
 mbsync -a
-mu init --maildir=~/email/ --my-address=872666026@qq.com --my-address=xucheng2@xxx.com
+mu init --maildir=~/email/ --my-address=872666026@qq.com --my-address=khpurat2@furva.pbz
 mu index
