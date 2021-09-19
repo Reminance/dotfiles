d5c494608580d13dba3707e17b66245c9bbf73f1
xucheng <872666026@qq.com>
2021-09-19 14:50:08 +0800

    update

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -374,31 +374,31 @@
 (use-package try)
 
 ;; evil
-(use-package evil
-  :init
-  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
-  (setq evil-want-keybinding nil)
-  :config
-  (eval-after-load "evil-maps"
-    (dolist (map '(evil-motion-state-map
-                   evil-normal-state-map
-                   evil-insert-state-map
-                   evil-visual-state-map
-                   evil-emacs-state-map))
-      (define-key (eval map) "\C-n" nil)
-      (define-key (eval map) "\C-p" nil)
-      (define-key (eval map) "\C-a" nil)
-      (define-key (eval map) "\C-e" nil)
-      (define-key (eval map) "\C-f" nil)
-      (define-key (eval map) "\C-b" nil)
-      (define-key (eval map) "\M-." nil)
-      ))
-  (evil-mode 1))
+;; (use-package evil
+;;   :init
+;;   (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+;;   (setq evil-want-keybinding nil)
+;;   :config
+;;   (eval-after-load "evil-maps"
+;;     (dolist (map '(evil-motion-state-map
+;;                    evil-normal-state-map
+;;                    evil-insert-state-map
+;;                    evil-visual-state-map
+;;                    evil-emacs-state-map))
+;;       (define-key (eval map) "\C-n" nil)
+;;       (define-key (eval map) "\C-p" nil)
+;;       (define-key (eval map) "\C-a" nil)
+;;       (define-key (eval map) "\C-e" nil)
+;;       (define-key (eval map) "\C-f" nil)
+;;       (define-key (eval map) "\C-b" nil)
+;;       (define-key (eval map) "\M-." nil)
+;;       ))
+;;   (evil-mode 1))
 
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
+;; (use-package evil-collection
+;;   :after evil
+;;   :config
+;;   (evil-collection-init))
 
 ;; magit
 (use-package magit
