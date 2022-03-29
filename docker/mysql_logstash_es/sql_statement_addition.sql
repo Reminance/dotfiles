9a6759875fe14a706ffbf0cb0fbcbd2034519995
xucheng <xucheng2@shein.com>
2022-03-30 00:19:25 +0800

    update

--- docker/mysql_logstash_es/sql_statement_addition.sql
+++ docker/mysql_logstash_es/sql_statement_addition.sql
@@ -1,7 +1,7 @@
 SELECT
-    id, name, DATE_FORMAT(update_time,'%Y-%m-%d %T') update_time
+    id, name, DATE_FORMAT(update_time,'%Y-%m-%d %H:%i:%s') update_time
 FROM user
 WHERE
-	update_time >= DATE_SUB(:sql_last_value, INTERVAL 5 MINUTE)
+	update_time >= DATE_SUB(:sql_last_value, INTERVAL 15 SECOND)
 ORDER BY
 	update_time ASC
