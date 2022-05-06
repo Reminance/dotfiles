c5975af8a03e141769f0bb9cfe2d76f5fa30b950
xucheng <xucheng2@shein.com>
2022-05-06 23:45:17 +0800

    update

--- .config/nvim/lua/plugin-settings.lua
+++ .config/nvim/lua/plugin-settings.lua
@@ -12,7 +12,7 @@ vim.g["rainbow_active"] = 1
 -- vim-snazzy
 vim.g["SnazzyTransparent"] = 1
 vim.cmd('colorscheme snazzy')
-vim.cmd('highlight Special guifg=#ff6ac1 gui=italic,underline,bold')  -- for telescope: highlight matching chars fg
+vim.cmd('highlight Special guifg=#ff6ac1 gui=italic,bold')  -- for telescope: highlight matching chars fg, gui=italic,underline,bold
 
 -- onedarkpro
 -- require('onedarkpro').load()
