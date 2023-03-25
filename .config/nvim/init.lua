206bbcb9c51878b2f8ef0446caed040ca06c4a5c
xucheng <xucheng2@shein.com>
2023-03-25 08:16:27 +0800

    add venn.nvim && diffview.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -302,6 +302,11 @@ require('packer').startup(function(use)
   -- file navigation
   use 'junegunn/fzf.vim'
 
+  -- Draw ASCII diagrams in Neovim.
+  use "jbyuki/venn.nvim"
+  -- diffview
+  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
+
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  requires = {
@@ -493,13 +498,44 @@ vim.keymap.set('n', '<Leader>gc', ':G commit<CR>', options)
 vim.keymap.set('n', '<Leader>gf', ':Git fetch<CR>', options)
 vim.keymap.set('n', '<Leader>gF', ':Git pull<CR>', options)
 vim.keymap.set('n', '<Leader>gp', ':Git push<CR>', options)
-vim.keymap.set('n', '<Leader>gd', ':Gvdiffsplit<CR>', options)
+-- vim.keymap.set('n', '<Leader>gd', ':Gvdiffsplit<CR>', options)
+vim.keymap.set('n', '<Leader>gd', ':DiffviewOpen<CR>', options) -- from diffview.nvim
 vim.keymap.set('n', '<Leader>gh', ':diffget //2<CR>', options)
 vim.keymap.set('n', '<Leader>gl', ':diffget //3<CR>', options)
 vim.keymap.set('n', '<Leader>gs', ':G<CR>', options)
 
 -- far
 vim.keymap.set('n', '<Leader>F', ':F  %<left><left>', options)
+
+-- venn.nvim, Draw ASCII diagrams in Neovim: enable or disable keymappings
+function _G.Toggle_venn()
+    local venn_enabled = vim.inspect(vim.b.venn_enabled)
+    if venn_enabled == "nil" then
+        vim.b.venn_enabled = true
+        vim.cmd[[setlocal ve=all]]
+        -- draw a line on HJKL keystokes
+        vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", {noremap = true})
+        -- draw a box by pressing "f" with visual selection
+        vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", {noremap = true})
+    else
+        vim.cmd[[setlocal ve=]]
+        vim.cmd[[mapclear <buffer>]]
+        vim.b.venn_enabled = nil
+    end
+end
+-- toggle keymappings for venn using <leader>v
+vim.api.nvim_set_keymap('n', '<leader>V', ":lua Toggle_venn()<CR>", { noremap = true})
+
+-- diffview.nvim
+-- Lua
+local actions = require("diffview.actions")
+
+require("diffview").setup({
+  enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
+})
 
 -- lsp config
 vim.diagnostic.config({
