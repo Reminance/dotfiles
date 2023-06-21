42662c59b0fe2ed3428ff7f48363736b1d80df99
xucheng <xucheng2@shein.com>
2023-06-21 09:34:33 +0800

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -599,7 +599,10 @@ else
     name = 'minimal',
     -- set_lsp_keymaps = true,
     set_lsp_keymaps = {preserve_mappings = false}, -- Change set_lsp_keymaps to this to force the keybindings from lsp-zero.
-    manage_nvim_cmp = true,
+    -- manage_nvim_cmp = true,
+    manage_nvim_cmp = {
+      set_extra_mappings = false,
+    },
     suggest_lsp_servers = false,
   })
   lsp.on_attach(function(client, bufnr)
@@ -643,6 +646,13 @@ else
     Copilot = "",
   }
   local cmp = require('cmp')
+  -- luasnip load
+  require('luasnip.loaders.from_vscode').lazy_load()
+  local luasnip = require('luasnip')
+  local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+  end
   cmp.setup({
     snippet = {
       -- REQUIRED - you must specify a snippet engine
@@ -658,6 +668,31 @@ else
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
+      ["<C-y>"] = cmp.config.disable,
+      -- ["<Tab>"] = cmp.config.disable,
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_locally_jumpable() then
+          luasnip.expand_or_jump()
+        elseif luasnip.jumpable(1) then
+          luasnip.jump(1)
+        elseif has_words_before() then
+          -- cmp.complete()
+          fallback()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
