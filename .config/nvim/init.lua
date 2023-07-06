ffbff9deaf25bc0486d47ebafa1b109b389755a4
xucheng <xucheng2@shein.com>
2023-07-07 00:00:25 +0800

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -818,7 +818,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>fm', function()
+    vim.keymap.set({ 'n', 'v' }, '<space>fm', function()
       vim.lsp.buf.format { async = true }
     end, opts)
   end,
