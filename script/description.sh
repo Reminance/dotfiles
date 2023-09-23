9d32a44b2afd1d8abf5dddd693606e8416be617b
xucheng <xucheng2@shein.com>
2023-09-23 08:38:57 +0800

    update

---
+++ script/description.sh
@@ -0,0 +1,17 @@
+该 GitHub 仓库是一个针对 Mac 和 Linux 环境的系统配置管理工具箱，包含一系列常用的开发工具和软件环境的配置文件、脚本和命令行工具，用于帮助用户快速搭建开发环境、提升工作效率和开发体验。其中主要包含以下内容：
+  1. Shell 配置文件：该仓库包含了如 zsh、bash 和 fish 等 Shell 的配置文件，用户可以借助这些文件来进行各种自定义 Shell 的配置。
+  2. Vim/Emacs 配置文件：用户可以使用这些配置文件来加速 Vim 和 Emacs 的使用，包括一些常用的插件和快捷键，进一步提升编辑器的功能和效率。
+  3. 窗口管理器配置文件：dwm、i3、yabai 等窗口管理器的配置文件，可以让用户快速的配置和使用自己的桌面环境，提高效率和舒适度。
+  4. 终端仿真器配置文件：Alacritty、iTerm2 等终端仿真器的配置文件，可以根据用户的需要，提供更美观、更快捷、更高效的终端使用体验。
+  5. Docker 配置文件：包括编排文件以及各种镜像的 Dockerfile 等，可以帮助用户部署和管理 Docker 镜像，创建与测试应用环境。
+  6. Python 开发环境配置文件：包含了 Python 开发所需的各种开发环境相关的配置文件，如 PIP 工具的配置文件、Jupyter Notebook 的配置文件等，可以让用户快速搭建完善的 Python 开发环境。
+  7. 工作脚本：包括各种 Python、Docker、数据库管理相关的 Shell 脚本和命令行工具，可以帮助用户快速处理日常工作中的任务。
+
+The repository is a system configuration management toolbox for Mac and Linux environments, containing a series of commonly used development tools and software environment configuration files, scripts and command line tools, used to help users quickly set up a development environment, improve work efficiency and development experience. It mainly contains the following contents:
+1. Shell configuration files: The repository contains configuration files for shells such as zsh, bash, and fish, and users can use these files to configure various customized shells.
+2. Vim/Emacs configuration files: Users can use these configuration files to speed up the use of Vim and Emacs, including some common plug-ins and shortcuts to further improve the function and efficiency of the editor.
+3. Window Manager configuration file: dwm, i3, yabai and other window manager configuration file, allowing users to quickly configure and use their own desktop environment, improve efficiency and comfort.
+4. Terminal emulator configuration file: Alacritty, iTerm2 and other terminal emulator configuration files, according to user needs, to provide a more beautiful, faster, and more efficient terminal experience.
+5. Docker configuration files: including orchestration files and Dockerfiles of various images, which can help users deploy and manage Docker images, create and test application environments.
+6. Python development environment configuration file: contains various development environment-related configuration files required by Python development, such as the configuration file of PIP tool, Jupyter Notebook configuration file, etc., which allows users to quickly build a perfect Python development environment.
+7. Working scripts: including a variety of Python, Docker, database management-related Shell scripts and command line tools, which can help users quickly deal with tasks in daily work.
